2aac752562c9942425ddd525423dfc84daa30dd6
Gem Barrett <gem@gembarrett.com>
2016-09-22 02:47:23 +0100

    got it showing splash page first

--- www/js/app.js
+++ www/js/app.js
@@ -31,13 +31,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('splash', {
+    url: '/',
+    templateUrl: 'templates/splash.html'
+  })
+
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
+  //
   // Each tab has its own nav history stack:
 
   .state('tab.dash', {
@@ -80,6 +85,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/');
 
 });
