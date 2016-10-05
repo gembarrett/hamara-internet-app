2eb00b2a7cdf552bf28c649f081ac5adc9323156
Gem Barrett <gem@gembarrett.com>
2016-10-05 14:47:43 +0100

    getting rid of dead code

--- www/js/services.js
+++ www/js/services.js
@@ -9,38 +9,5 @@ angular.module('starter.services', ['ngResource'])
         method: 'GET'
       });
     },
-    // getCats: function(cId) {
-    //   // get the json array of category objects
-    //   var cats = content.query();
-    //   cats.$promise.then(function(data){
-    //       // for each category object
-    //       for (var i = 0; i < cats.length; i++) {
-    //         // if the category matches the one we're looking for
-    //         if (cats[i].cId === cId) {
-    //           // return that category object
-    //           console.log(cats[i]);
-    //           return cats[i];
-    //         }
-    //       }
-    //       return null;
-    //     });
-    //   },
-      // getSubcats: function(cId) {
-      //   // get the json array of category objects
-      //   var subcats = content.query();
-      //   var dataReturned;
-      //   subcats.$promise.then(function(data){
-      //       // for each category object
-      //       for (var i = 0; i < subcats.length; i++) {
-      //         // if the category matches the one we're looking for
-      //         if (subcats[i].cId === cId) {
-      //           // return that category object
-      //           console.log(subcats[i].subcats);
-      //           return subcats;
-      //         }
-      //       }
-      //       return null;
-      //     });
-      //   }
-    }
+  }
 });
