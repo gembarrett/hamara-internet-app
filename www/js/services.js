a4582c002c1786b4f230e5474fc73a0ec41e813b
Gem Barrett <gem@gembarrett.com>
2016-10-05 04:23:17 +0100

    don't look now but I think I might be getting somewhere

--- www/js/services.js
+++ www/js/services.js
@@ -6,20 +6,21 @@ angular.module('starter.services', ['ngResource'])
     all: function() {
       return content.query();
     },
-    get: function(id) {
-      console.log('get');
+    get: function(thisId) {
       // get the json array of category objects
       var cats = content.query();
-      // for each category object
-      for (var i = 0; i < cats.length; i++) {
-        // if the category matches the one we're looking for
-        if (cats[i].id === parseInt(id)) {
-          // return that category object
-          return cats[i];
-        }
+      cats.$promise.then(function(data){
+          // for each category object
+          for (var i = 0; i < cats.length; i++) {
+            // if the category matches the one we're looking for
+            if (cats[i].id === parseInt(thisId)) {
+              // return that category object
+              console.log(cats[i]);
+              return cats[i];
+            }
+          }
+          return null;
+        });
       }
-      return null;
     }
-
-  }
 });
