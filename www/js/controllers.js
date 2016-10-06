142431e03b8aa1fe031efdd20b1e25fbf7b3695a
Gem Barrett <gem@gembarrett.com>
2016-10-06 02:45:51 +0100

    it's a bit hacky but it's ok :)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,7 +15,7 @@ angular.module('starter.controllers', ['starter.services'])
 })
 
 // find the subcategories
-.controller('SubcatsCtrl', function($scope, $rootScope, $state, $stateParams, Content) {
+.controller('SubcatsCtrl', function($scope, $ionicHistory, $rootScope, $state, $stateParams, Content) {
   if ($rootScope.cats != undefined) {
     var cats = $rootScope.cats;
     var subcats = [];
@@ -25,7 +25,7 @@ angular.module('starter.controllers', ['starter.services'])
       // if the category matches the one we're looking for
       if (cats[i].cId === $stateParams.cId){
         $scope.subcats = cats[i].subcats;
-        console.log(subcats);
+        $rootScope.subcats = $scope.subcats;
         // return that category object
         return $scope.subcats;
       }
@@ -34,18 +34,43 @@ angular.module('starter.controllers', ['starter.services'])
     Content.all().then(function(cats){
       $rootScope.cats = cats.data;
       $scope.cats = cats.data;
-      $state.go('categories');
+      $ionicHistory.clearCache().then(function(){ $state.go('app') });
     });
   }
 
 })
 
 // find the subcategories
-.controller('ContentCtrl', function($scope, $rootScope, $state, $stateParams, Content) {
+.controller('ContentCtrl', function($scope, $ionicHistory, $rootScope, $state, $stateParams, Content) {
+  if ($rootScope.subcats === '' || $rootScope.subcats === undefined) {
+    $ionicHistory.clearCache().then(function(){ $state.go('app') });
+  }
   if ($state.params.subcatid != '' && $state.params.subcatid != undefined) {
     $rootScope.chosenSubcat = $state.params.subcatid;
   }
   $scope.subcatid = $rootScope.chosenSubcat;
+  $scope.currentTab = $state.current.name;
+  console.log($rootScope.subcats);
+  for (var i=0; i<$rootScope.subcats.length; i++){
+    if ($rootScope.subcats[i].sId === $scope.subcatid){
+      switch ($scope.currentTab) {
+        case '4tab.problem':
+          $scope.content = $rootScope.subcats[i].problem;
+          break;
+        case '4tab.solution':
+          $scope.content = $rootScope.subcats[i].solution;
+          break;
+        case '4tab.tips':
+          $scope.content = $rootScope.subcats[i].tips;
+          break;
+        case '4tab.problem':
+          $scope.resources = $rootScope.subcats[i].resources;
+          break;
+        default:
+          console.log('idk wtf is going on');
+      }
+    }
+  }
 
   // console.log($rootScope.cats);
   // if ($rootScope.cats != undefined) {
