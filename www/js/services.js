86ecfec52da3ae8877bf0cc414f3270a0b51745d
Gem Barrett <gem@gembarrett.com>
2016-10-31 03:08:49 +0000

    more work on translation, adding language class to the body

--- www/js/services.js
+++ www/js/services.js
@@ -6,8 +6,16 @@ angular.module('starter.services', ['ngResource'])
       var content;
       if (language === 'en') {
         content = 'appdata/data-en.json';
+        var bdy = document.getElementsByTagName('body');
+        console.log(bdy[0]);
+        bdy[0].classList.remove("pk-lang");
+        bdy[0].classList.toggle("en-lang");
       } else {
         content = 'appdata/data-pk.json';
+        var bdy = document.getElementsByTagName('body');
+        console.log(bdy[0]);
+        bdy[0].classList.remove("en-lang");
+        bdy[0].classList.toggle("pk-lang");
       }
       return $http({
         url: content,
