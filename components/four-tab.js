d14644d0b63000e17528cc6c003ab460fb9183ec
Gem Barrett <gem@gembarrett.com>
2017-11-05 14:58:32 -0500

    Preparing for four-tabbed pages

---
+++ components/four-tab.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class FourTab extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>This is Four Tab calling...</Text>
+      </View>
+    );
+  }
+}
