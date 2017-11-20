06d53f02cb78b40cf152e6422412fc3126e52950
Gem Barrett <gem@gembarrett.com>
2017-11-19 23:00:48 -0500

    started adding tabs in

--- components/about.js
+++ components/about.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+import ScrollableTabView, { DefaultTabBar, } from 'react-native-scrollable-tab-view';
 
 export default class AboutScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>About Screen</Text>
-      </View>
+      <ScrollableTabView style={{marginTop: 20}} initialPage={0} renderTabBar={() => <DefaultTabBar />} >
+        <Text tabLabel='Problem'>My</Text>
+        <Text tabLabel='Solution'>favorite</Text>
+        <Text tabLabel='Tips'>project</Text>
+        <Text tabLabel='Resources'>favorite</Text>
+      </ScrollableTabView>
     );
   }
 };
