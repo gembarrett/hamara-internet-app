0453f44b99eef8d374a1d5b8205d15459d688a64
Gem Barrett <gem@gembarrett.com>
2017-11-20 23:32:49 -0500

    trying to pull through content for Google Yourself tabs

--- components/four/search.js
+++ components/four/search.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import FourTab from '../four-tab.js';
+import { search } from '../../content/online.js';
 
 export default class SearchScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Google Yourself',
+  }
+  const tab1 = search[0].textEN;
+  const tab2 = search[1].textEN;
+  const tab3 = search[2].textEN;
+  const tab4 = search[3].textEN;
   render() {
     return (
-      <FourTab />
+        <FourTab />
     );
   }
-};
+}
---
+++ content/online.js
@@ -0,0 +1,87 @@
+const search = [
+  {
+    id: 1,
+    route: "Problem",
+    textEN: "Digital shadows are made up of the information websites collect about us every time we go online. Other people can use those details maliciously. Doxing is when our private information, such as phone number or address, is made public online in order to harass us.",
+    textPK: "جب بھی ہم آن لائن ہوتے ہیں تو ویب سائٹس ہماری معلومات اکٹھی کر لیتی ہیں ان معلومات کو ڈیجیٹل شیڈوز کہتے ہیں۔ کوئی بھی دوسرا شخص ہماری ان تفصیلات کو برائی کی نیت سے استعمال کر سکتا ہے۔ کسی کی ذاتی معلومات جیسے فون نمبر یا کسی کا پتہ وغیرہ کی بری نیت سے تشہیر کے جائے تو اسے ڈاکسنگ کہتے ہیں۔"
+  },
+  {
+    id: 2,
+    route: "Solution",
+    textEN: "Find out what your digital shadow looks like so you know what can be used against you. To do this, try self-doxing; use a search engine like DuckDuckGo, which protects your privacy, to search for details like your usernames, phone number and email addresses.",
+    textPK: "یہ جاننے کی کوشش کریں کہ آپ ڈیجیٹل شیڈو کیسا دکھائی دیتا ہےتاکہ آپ کو پتہ چل سکے کہ آپ کی کن معلومات کا غلط استعمال کیا جاسکتا ہے۔ یہ جاننے کیلئے اپنی ہی ڈاکسنگ کرنے کی کوشش کریں۔ اپنے یوزر   جیسے کسی بھی سرچ انجنDuckDuckGoنیم، فون نمبر، اور ای میل ایڈریسز جیسی تفصیلات کو جاننے کیلئے کو استعمال کریں جو آپ کی نجی معلومات کی حفاظت کرتا ہو۔"
+  },
+  {
+    id: 3,
+    route: "Tips",
+    textEN: "Self-dox regularly. Knowing what your digital shadow looks like can help you to make better decisions about the information you put online. <hr \/>Search for all information that could be used to identify or trace you, including: <ul><li>all your usernames on active profiles as well as inactive/past online profiles<\/li><li>all email addresses to see where your email has been posted or possibly misused<\/li><li>your phone number(s) including landlines and mobile phones<\/li><li>your home and family addresses<\/li><\/ul>Do a reverse image lookup, using a tool like Google Image Search or TinEye, on your public pictures, such as Facebook profile photos to see if they have been used anywhere else.",
+    textPK: "<p>اپنی خود کی ڈاکسنگ با قاعدگی سے کریں، یہ جاننا کہ آپ کا ڈیجیٹل شیڈو کیسا دکھائی دیتا ہےآپ کو اپنی آن لائن دی گئی معلومات سے متعلق بہتر فیصلے کرنے میں مدد دے سکتا ہے۔ <hr \/>اپنی ان تمام معلومات کیلئے سرچ کیجئے جن کو آپکا تعاقب کرنے اور آپکی شناخت حاصل کرنے میں استعمال کیا جا سکتا ہے۔ ان معلومات میں شامل ہیں:<ul><li>۔ آپکی غیر فعال آن لاےن پروفائل کی طرح آپکی فعال ای پروفائلز پر آپکے تمام یوزر نیم۔<\/li><li>۔ آپکے تمام ای میل ایڈریسز، یہ دیکھنے کیلئے کہ آپکی ای میلز کہاں پوسٹ ہوئی ہیں، یا ان کا ممکنہ ناجائز استعمال کیا گیا ہو۔<\/li><li>۔ آپکا فون بشمول آپکے موبائل فون اور آپکا زمینی فون نمبر۔<\/li><li>۔ آپکے گھر اور خاندان کے ایڈریسز۔<\/li><\/ul> TinEyeیا Google Image Service فیس بک پروفائل تصاویر جیسی اپنی تشہیری تصاویر پر <\/p><p>جیسا کوئی آلہ استعمال کرتے ہوئےریورس امیج لک اپ کریں تاکہ یہ دیکھا جائے کہ آیا ان کا کہیں استعمال کیا گیا ہے۔ <\/p>"
+  },
+  {
+    id: 4,
+    route: "Resources",
+    links: [
+      {
+        url: 'https://myshadow.org/',
+        textEN: 'Learn more about digital shadows',
+        textPK: ' ڈیجیٹل شیڈوز کے بارے میں مزید جاننے کیلئے'
+      }
+    ]
+  }
+];
+
+const browsers = [
+  {
+    id: 1,
+    route: "Problem",
+    textEN: "Google yourself",
+    textPK: "Google yourself"
+  },
+  {
+    id: 2,
+    route: "Solution",
+    textEN: "Browsers",
+    textPK: "براوزرز"
+  },
+  {
+    id: 3,
+    route: "Tips",
+    textEN: "Hiding your location",
+    textPK: "اپنے مقام کو چھپانا"
+  },
+  {
+    id: 4,
+    route: "Resources",
+    textEN: "Hiding your location",
+    textPK: "اپنے مقام کو چھپانا"
+  }
+];
+
+const location = [
+  {
+    id: 1,
+    route: "Problem",
+    textEN: "Google yourself",
+    textPK: "Google yourself"
+  },
+  {
+    id: 2,
+    route: "Solution",
+    textEN: "Browsers",
+    textPK: "براوزرز"
+  },
+  {
+    id: 3,
+    route: "Tips",
+    textEN: "Hiding your location",
+    textPK: "اپنے مقام کو چھپانا"
+  },
+  {
+    id: 4,
+    route: "Resources",
+    textEN: "Hiding your location",
+    textPK: "اپنے مقام کو چھپانا"
+  }
+];
+
+export {search, browsers, location};
