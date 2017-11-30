789012c7967807ecc96b398e21064b67714657b2
Gem Barrett <gem@gembarrett.com>
2017-11-30 00:16:30 -0500

    report content is inside working accordion

--- components/report.js
+++ components/report.js
@@ -4,44 +4,54 @@ import Accordion from '@ercpereda/react-native-accordion';
 import { reportTo } from '../content/report.js';
 import { prefs } from '../routes/prefs.js';
 
-const Header = ({ isOpen }) =>
-  <View style={{
-      paddingTop: 15,
-      paddingRight: 15,
-      paddingLeft: 15,
-      paddingBottom: 15,
-      borderBottomWidth: 1,
-      borderBottomColor: '#a9a9a9',
-      backgroundColor: '#f9f9f9',
-    }}>
-      <Text>{`${isOpen ? '-' : '+'} Click to Expand`}</Text>
-    </View>;
+export default class ReportScreen extends React.Component {
+  get accordion() {
+    var accordionArr = [];
+    for (let i = 0; i < reportTo.length; i++){
+      const title = prefs.language === 'pk' && reportTo[i].titlePK ? reportTo[i].titlePK : reportTo[i].titleEN;
+      const Header = ({ isOpen }) =>
+        <View style={{
+            paddingTop: 15,
+            paddingRight: 15,
+            paddingLeft: 15,
+            paddingBottom: 15,
+            borderBottomWidth: 1,
+            borderBottomColor: '#a9a9a9',
+            backgroundColor: '#f9f9f9',
+          }}>
+            <Text>{`${isOpen ? '-' : '+'} ${title}`}</Text>
+          </View>;
+      const content = prefs.language === 'pk' && reportTo[i].textPK ? reportTo[i].textPK : reportTo[i].textEN;
+      const Content = (
+        <View style={{
+            display: 'flex',
+            backgroundColor: '#31363D'
+          }}>
+            <Text style={{
+              paddingTop: 15,
+              paddingRight: 15,
+              paddingBottom: 15,
+              paddingLeft: 15,
+              color: '#fff',
+            }}>
+              {content}
+            </Text>
+          </View>);
+      accordionArr.push(
+          <Accordion key={i}
+            header={Header}
+            content={Content}
+            duration={300}
+          />
+      )
+    }
+    return accordionArr;
+  }
 
-const Content = (
-  <View style={{
-      display: 'flex',
-      backgroundColor: '#31363D'
-    }}>
-      <Text style={{
-        paddingTop: 15,
-        paddingRight: 15,
-        paddingBottom: 15,
-        paddingLeft: 15,
-        color: '#fff',
-      }}>
-        This content is hidden in the accordion
-      </Text>
-    </View>);
-
-export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Accordion
-          header={Header}
-          content={Content}
-          duration={300}
-        />
+        {this.accordion}
       </View>
     );
   }
@@ -51,7 +61,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: '100%'
+  }
 });
