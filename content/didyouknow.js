c1aff42250175e38daf71699c9b1e32e8b70ca62
Gem Barrett <gem@gembarrett.com>
2017-12-06 23:53:02 -0500

    added content for didyoukow section

---
+++ content/didyouknow.js
@@ -0,0 +1,76 @@
+const didyouknow = [
+  {
+    titleEN: "Did You Know?",
+    titlePK: "Did You Know?"
+  },
+  {
+    textEN: "70% of women don’t report cyber harassment to law enforcement agencies when they are harassed *",
+    textPK: "70% of women don’t report cyber harassment to law enforcement agencies when they are harassed *"
+  },
+  {
+    textEN: "47% women believe that their complaints won’t be taken seriously when they go to the LEAs *",
+    textPK: "47% women believe that their complaints won’t be taken seriously when they go to the LEAs *"
+  },
+  {
+    textEN: "45% women believe that reporting harassment to LEA’s is embarrassing *",
+    textPK: "45% women believe that reporting harassment to LEA’s is embarrassing *"
+  },
+  {
+    textEN: "72% of women are not aware of any laws relating to cyber harassment in Pakistan **",
+    textPK: "72% of women are not aware of any laws relating to cyber harassment in Pakistan **"
+  },
+  {
+    textEN: "Only 51% understand a little about the terms and conditions of social media websites **",
+    textPK: "Only 51% understand a little about the terms and conditions of social media websites **"
+  },
+  {
+    textEN: "Among girls below 18 years of age, about 55% women use smartphones **",
+    textPK: "Among girls below 18 years of age, about 55% women use smartphones **"
+  },
+  {
+    textEN: "67% of women use Facebook on the internet according to our sample group**",
+    textPK: "67% of women use Facebook on the internet according to our sample group**"
+  },
+  {
+    textEN: "About 81% women above the age of 25 use Facebook **",
+    textPK: "About 81% women above the age of 25 use Facebook **"
+  },
+  {
+    textEN: "79% of women use their devices regularly **",
+    textPK: "79% of women use their devices regularly **"
+  },
+  {
+    textEN: "According to our study, 50% women identified Facebook as the worst platform for online harassment **",
+    textPK: "According to our study, 50% women identified Facebook as the worst platform for online harassment **"
+  },
+  {
+    textEN: "70% of women are afraid of posting pictures online because they could be misused **",
+    textPK: "70% of women are afraid of posting pictures online because they could be misused **"
+  },
+  {
+    textEN: "25% of women have witnessed a girl being bullied by men online **",
+    textPK: "25% of women have witnessed a girl being bullied by men online **"
+  },
+  {
+    textEN: "40% of women had been stalked and harassed via messaging apps **",
+    textPK: "40% of women had been stalked and harassed via messaging apps **"
+  },
+  {
+    textEN: "48% of women knew someone who had stopped using the internet after being harassed online **",
+    textPK: "48% of women knew someone who had stopped using the internet after being harassed online **"
+  },
+  {
+    notes: [
+      {
+        textEN: "* Cyber Harassment Helpline 6 months report",
+        textPK: "* Cyber Harassment Helpline 6 months report"
+      },
+      {
+        textEN: "** Measuring Pakistani Women’s Experience of Online Violence",
+        textPK: "** Measuring Pakistani Women’s Experience of Online Violence"
+      }
+    ]
+  }
+];
+
+export {didyouknow};
