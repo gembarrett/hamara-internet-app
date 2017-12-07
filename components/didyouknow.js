e50b7ff8a5fc5f4a1ed5585cda14bf056a2b9cdf
Gem Barrett <gem@gembarrett.com>
2017-12-06 23:53:36 -0500

    added list component to display points

--- components/didyouknow.js
+++ components/didyouknow.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, ListItem, List } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+import { didyouknow } from '../content/didyouknow.js';
+import { translatedText, translatedTitle } from '../routes/shared.js';
 
 export default class DidYouKnowScreen extends React.Component {
+  static navigationOptions = {
+    title: translatedTitle(didyouknow, 0)
+  };
+  get content() {
+    var contentArr = [];
+    for (let i = 1; i < didyouknow.length; i++) {
+      if (didyouknow[i].notes) {
+        console.log(didyouknow[i].notes);
+      } else {
+        contentArr.push({key: {i}, text: translatedText(didyouknow, i)});
+      }
+    }
+    return contentArr;
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>DidYouKnow Screen</Text>
+      <FlatList
+        data={this.content}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
       </View>
     );
   }
