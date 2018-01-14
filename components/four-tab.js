0b4f297bc9fc1a78460e7174820c0011d7591b25
Gem Barrett <gem@gembarrett.com>
2018-01-14 01:46:26 -0500

    started styling tabs, beginning with text colour and background

--- components/four-tab.js
+++ components/four-tab.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import ScrollableTabView, { DefaultTabBar, } from 'react-native-scrollable-tab-view';
+import { globals } from '../styles/globals.js';
 
 export default class FourTab extends React.Component {
   render() {
     return (
-      <ScrollableTabView style={{marginTop: 20}} initialPage={0} renderTabBar={() => <DefaultTabBar />} >
-        <Text tabLabel='Problem'>{this.props.tab1}</Text>
-        <Text tabLabel='Solution'>{this.props.tab2}</Text>
-        <Text tabLabel='Tips'>{this.props.tab3}</Text>
-        <Text tabLabel='Resources'>{this.props.tab4}</Text>
+      <ScrollableTabView tabBarActiveTextColor="#fff" tabBarInactiveTextColor="#eee" style={[globals.green, globals.base]} initialPage={0} renderTabBar={() => <DefaultTabBar />} >
+        <Text style={globals.whiteText} tabLabel='Problem'>{this.props.tab1}</Text>
+        <Text style={globals.whiteText} tabLabel='Solution'>{this.props.tab2}</Text>
+        <Text style={globals.whiteText} tabLabel='Tips'>{this.props.tab3}</Text>
+        <Text style={globals.whiteText} tabLabel='Resources'>{this.props.tab4}</Text>
       </ScrollableTabView>
     );
   }
