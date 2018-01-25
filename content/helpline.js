5a391f021c85a22a85a5b313d78908a1b34d3516
Gemma Barrett <geminimac@geminimac.local>
2018-01-25 15:52:02 -0500

    added translations for helpline section

--- content/helpline.js
+++ content/helpline.js
@@ -1,38 +1,38 @@
 const helpline = [
   {
     titleEN: "Cyber Harassment Helpline",
-    titlePK: "پاس ورڈز",
+    titlePK: "سائبر ہراسمنٹ ہیلپ لائن",
   },
   {
     textEN: "The Cyber Harassment Helpline is Pakistan’s first dedicated helpline addressing issues of online abuse and violence providing a free, safe, gender-sensitive and confidential service. The Helpline Support Staff gives legal advice, digital security support and psychological counselling to victims of online harassment. The Helpline was launched on December 1, 2016.",
-    textPK: "The Cyber Harassment Helpline is Pakistan’s first dedicated helpline addressing issues of online abuse and violence providing a free, safe, gender-sensitive and confidential service. The Helpline Support Staff gives legal advice, digital security support and psychological counselling to victims of online harassment. The Helpline was launched on December 1, 2016."
+    textPK: "سائبر ہراسمنٹ ہیلپ لائن پاکستان کی پہلی ہیلپ لائن ہے جو آن لائن بد سلوکی، تشدد اور  ہراساں کرنے جیسے مسائل کو اجاگر کرنے اور ان مسائل سے نبرد آزما ہونے کیلئے مفت، محفوظ، صنفی حساسیت کو ملحوظ رکھتے ہوئے ایک رازدارانہ خدمت مہیا کرنے کیلئے وقف ہے کی گئی ہے۔ ہیلپ لائن کا امدادی سٹاف آن لائن ہراسانی کا شکار ہوئے لوگوں کو قانونی مشاورت، ڈیجیٹل حفاظتی امداد اور نفسیاتی رہنمائی فراہم کرتا ہے۔یہ ہیلپ لائن یکم دسمبر، ۲۰۱۶ میں قائم کی گئی۔"
   },
   {
     textEN: "The toll free number [0800-39393] is available to people looking for help between 9am till 5pm, 7 days a week. The Support Staff can also be contacted via email at helpdesk@digitalrightsfoundation.pk and our Facebook page https://www.facebook.com/DigitalRightsFoundation/",
-    textPK: "The toll free number [0800-39393] is available to people looking for help between 9am till 5pm, 7 days a week. The Support Staff can also be contacted via email at helpdesk@digitalrightsfoundation.pk and our Facebook page https://www.facebook.com/DigitalRightsFoundation/"
+    textPK: "، ہفتہ کے ۷ دن، صبح ۹ سے شام ۵ بجے تک ان لوگوں کیلئے دستیاب ہے جنہیں 0800-39393 اس ہیلپ لائن کا ٹال فری نمبر helpdesk@digitalrightsfoundation.pkمدد درکار ہو۔  اس کے علاوہ امدادی سٹاف سے بذریعہ ای میل  پر بھی رابطہ کیا https://www,facebook.com/DigitalRightsFoundation/فیس بک پیج  اور  جا سکتا ہے۔"
   },
   {
     links: [
       {
         url: "http://digitalrightsfoundation.pk/wp-content/uploads/2017/04/4-Month-Report.Final_.pdf",
         textEN: "Cyber Harassment Helpline 4 months report",
-        textPK: "Cyber Harassment Helpline 4 months report"
+        textPK: "سائبر ہراسمنٹ ہیلپ لائن کی چار ماہ کی کاردگی کی رپورٹ"
       },
       {
         url: "https://digitalrightsfoundation.pk/wp-content/uploads/2017/07/Cyber-Harassment-Helpline-Six-Month-Report.pdf",
         textEN: "Cyber Harassment Helpline 6 months report",
-        textPK: "Cyber Harassment Helpline 6 months report"
+        textPK: "سائبر ہراسمنٹ ہیلپ لائن کی ششماہی کارکردگی کی رپورٹ"
       },
       {
         url: "http://digitalrightsfoundation.pk/wp-content/uploads/2017/05/Hamara-Internet-Online-Harassment-Report.pdf",
         textEN: "Add gendered aspect to harassment report",
-        textPK: "Add gendered aspect to harassment report"
+        textPK: "ہراسمنٹ رپورٹ میں جنسی پہلو کا اضافہ"
       },
     ]
   },
   {
     textEN: "Digital Rights Foundation has trained up to 1800 women under the Hamara Internet banner all across Pakistan. The report has data collections from 17 sessions conducted in Punjab, Sindh, Balochistan, Khyber Pakhtunkhuwa and Gilgit Baltistan. The study focuses on the different aspects of online harassment, violence against women and how women use digital tools as a whole.",
-    textPK: "Digital Rights Foundation has trained up to 1800 women under the Hamara Internet banner all across Pakistan. The report has data collections from 17 sessions conducted in Punjab, Sindh, Balochistan, Khyber Pakhtunkhuwa and Gilgit Baltistan. The study focuses on the different aspects of online harassment, violence against women and how women use digital tools as a whole."
+    textPK: "ڈیجیٹل رائٹس فاؤنڈیشن نے ہمارا انٹرنیٹ تحریک کے تحت اب تک پاکستان بھر میں ۱۸۰۰ سے زائد خواتین کو  تربیت دی ہے۔ رپورٹ میں ان تمام ۱۷ تقاریب کا ذکر کیا گیا ہے جو پنجاب، سندھ، بلوچستان، خیبر پختونخوا اور گلگت بلتستان میں منعقد کروائی گئیں۔ یہ تحریر ان تمام پہلوؤں پر نظر ڈالتی ہے جو آن لائن ہراسانی، خواتین کے خلاف تشدد پر مبنی ہیں اور انہیں اس سے یہ بھی پتہ چل جائے گا کہ خواتین کیسے ڈیجیٹل ذرائع کا استعمال کریں۔"
   }
 ];
 
