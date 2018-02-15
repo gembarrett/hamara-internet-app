90bb6d70d9a26cae2b43209f09ff0f29bd97b7c2
Gemma Barrett <geminimac@geminimac.local>
2018-02-15 01:38:40 -0500

    support content added

--- components/support.js
+++ components/support.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+import { translatedText, translatedTitle } from '../routes/shared.js';
+import { support } from '../content/support.js';
 
 export default class SupportScreen extends React.Component {
+  static navigationOptions = {
+    title: translatedTitle(support, 0)
+  };
+  get content() {
+    var contentArr = [];
+    for (let i = 1; i < support.length; i++) {
+        // change to print both
+        var name = support[i].name;
+        var number = support[i].number;
+        contentArr.push({key: name, text: number });
+    }
+    return contentArr;
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Support Screen</Text>
-        PCSW - 1043
-Rozan helpline- 0800-22444
-Cyber Harassment helpline- 0800-39393
-PTA- 0800-55055
-Rescue- 1122
-Police emergency- 15
-FIA- 9911
+      <FlatList
+        data={this.content}
+        renderItem={({item}) => <Text>{item.key} - {item.text}</Text>}
+      />
 
       </View>
     );
---
+++ content/support.js
@@ -0,0 +1,36 @@
+const support = [
+  {
+    titleEN: "I need support",
+    titlePK: "مجھے مدد چاہئے"
+  },
+  {
+    name: "PCSW",
+    number: "1043"
+  },
+  {
+    name: "Rozan helpline",
+    number: "0800-22444"
+  },
+  {
+    name: "Cyber Harassment helpline",
+    number: "0800-39393"
+  },
+  {
+    name: "PTA",
+    number: "0800-55055"
+  },
+  {
+    name: "Rescue",
+    number: "1122"
+  },
+  {
+    name: "Police emergency",
+    number: "15"
+  },
+  {
+    name: "FIA",
+    number: "9911"
+  }
+];
+
+export { support };
