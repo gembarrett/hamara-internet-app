bb7300c55d8249cd664cf1fcdf24e34a421a15ba
Gemma Barrett <geminimac@geminimac.local>
2018-02-16 18:56:01 -0500

    calling support numbers now works

--- components/support.js
+++ components/support.js
@@ -10,10 +10,12 @@ export default class SupportScreen extends React.Component {
   };
   get content() {
     var contentArr = [];
+    // var canCall = Linking.canOpenURL(support[1].number) ? true : false;
     for (let i = 1; i < support.length; i++) {
         var name = support[i].name;
         var number = support[i].number;
-        contentArr.push({key: name, text: number });
+        var tel = 'tel:' + support[i].number;
+        contentArr.push({key: name, text: number, tel: tel });
     }
     return contentArr;
   }
@@ -35,12 +37,12 @@ export default class SupportScreen extends React.Component {
           ({item}) =>
             <View>
               <Text key={item.key}>
-                {item.key} - {item.text}
+                {item.key}
               </Text>
               <Text>
-              {this.number(item.text)}
+                {item.text}
               </Text>
-              <Button title="call {item.key}" onPress={() => 'tel:+14379814406'}>
+              <Button title={'call ' + item.key} onPress={() => Linking.openURL(item.tel)}>
               </Button>
             </View>
           }
