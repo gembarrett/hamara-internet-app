100f715c54faddad6eed12fa89806b86e30a3805
Gemma Barrett <geminimac@geminimac.local>
2018-03-20 00:07:53 -0400

    adding new white font to tab

--- components/four-tab.js
+++ components/four-tab.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import ScrollableTabView, { DefaultTabBar, } from 'react-native-scrollable-tab-view';
+import BasicText from './sub/basicText.js';
+import CustomTabBar from './sub/customTabBar.js'
 import { globals } from '../styles/globals.js';
 
 export default class FourTab extends React.Component {
   render() {
     return (
-      <ScrollableTabView tabBarActiveTextColor="#fff" tabBarInactiveTextColor="#eee" style={[globals.green, globals.base, globals.menuButtons]} initialPage={0} renderTabBar={() => <DefaultTabBar />} >
-        <Text style={globals.whiteText} tabLabel='Problem'>{this.props.tab1}</Text>
-        <Text style={globals.whiteText} tabLabel='Solution'>{this.props.tab2}</Text>
-        <Text style={globals.whiteText} tabLabel='Tips'>{this.props.tab3}</Text>
-        <Text style={globals.whiteText} tabLabel='Resources'>{this.props.tab4}</Text>
+      <ScrollableTabView style={[globals.green, globals.base]} initialPage={0} renderTabBar={() => <CustomTabBar />} >
+        <BasicText tabLabel='Problem'>{this.props.tab1}</BasicText>
+        <BasicText tabLabel='Solution'>{this.props.tab2}</BasicText>
+        <BasicText tabLabel='Tips'>{this.props.tab3}</BasicText>
+        <BasicText tabLabel='Resources'>{this.props.tab4}</BasicText>
       </ScrollableTabView>
     );
   }
