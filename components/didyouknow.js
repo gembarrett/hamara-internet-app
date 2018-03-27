36a8671ae4e3276d5b9db9e769472aabda71bf77
Gemma Barrett <geminimac@geminimac.local>
2018-03-26 23:21:16 -0400

    notes now fully added to didyouknow section

--- components/didyouknow.js
+++ components/didyouknow.js
@@ -15,8 +15,10 @@ export default class DidYouKnowScreen extends React.Component {
     for (let i = 1; i < didyouknow.length; i++) {
       if (didyouknow[i].notes) {
         // change to print both
-        var text = didyouknow[i].notes[0].textEN;
-        contentArr.push({key: {i}, text: text });
+        for (let j=0; j < didyouknow[i].notes.length; j++) {
+          var text = didyouknow[i].notes[0].textEN;
+          contentArr.push({key: {i}, text: translatedText(didyouknow[i].notes, j) });
+        }
       } else {
         contentArr.push({key: {i}, text: translatedText(didyouknow, i)});
       }
