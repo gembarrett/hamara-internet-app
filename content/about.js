d3521f1a3b42cf0336f07bb2c67d746de9473fcb
Gemma Barrett <geminimac@geminimac.local>
2018-06-12 17:43:45 -0400

    added Urdu translations to Hamara Internet section #83

--- content/about.js
+++ content/about.js
@@ -16,38 +16,44 @@ const about = [
       {
         icon: "shield",
         titleEN: "Protection against Digital Abuse",
+        titlePK: "ڈیجیٹل بد سلوکی سے حفاظت",
         textEN: "To protect women against cyber harassment and to promote better understanding of using safe and secure Internet.",
-        textPK: "To protect women against cyber harassment and to promote better understanding of using safe and secure Internet."
+        textPK: "خواتین کو سائبر ہراسگی سے بچانا اور انٹرنیٹ کے محفوظ استعمال  کی بہتر عوامل کو آگے بڑھانا۔"
       },
       {
         icon: "puzzle-piece",
         titleEN: "Capacity Building",
+        titlePK: "صلاحیتوں کو اجاگر کرنا",
         textEN: "To enable women to make their mark on the Internet and be more active on social media, in turn empowering women to embrace the online space and showcase their talent.",
-        textPK: "To enable women to make their mark on the Internet and be more active on social media, in turn empowering women to embrace the online space and showcase their talent."
+        textPK: "خواتین کو اس قابل بنانا کہ وہ انٹرنیٹ پہ اپنا مقام حاصل کریں اور سوشل میڈیا پہ مزید مستعد ہوجائیں، تاکہ ان کی صلاحیتیں اجاگر ہونے کے ساتھ ساتھ آن لائن سپیس میں انہیں خود مختاری بھی حاصل ہو۔"
       },
       {
         icon: "female",
         titleEN: "Equitable Access to ICTs",
+        titlePK: "تک رسائیICTsبرابری کی بنیاد پر",
         textEN: "To fight for equitable access to Information and Communication Technologies in Pakistan.",
-        textPK: "To fight for equitable access to Information and Communication Technologies in Pakistan."
+        textPK: "پاکستان میں معلومات و مواصلات ٹیکنالوجیز تک جنسی برابری کی بنیاد پر رسائی حاصل کرنے کیلئے کوشاں رہنا۔"
       },
       {
         icon: "book",
         titleEN: "Fostering Digital Literacy",
+        titlePK: "ڈیجیٹل تعلیم کو فروغ دینا",
         textEN: "To provide women with the right tools to express themselves with on the Internet and make them digital-savvy so that not only can they themselves utilize the digital space properly but also help their friends and families with it.",
-        textPK: "To provide women with the right tools to express themselves with on the Internet and make them digital-savvy so that not only can they themselves utilize the digital space properly but also help their friends and families with it."
+        textPK: "خواتین کو انٹرنیٹ پر اپنے خیالات کے اظہار کیلئے مناسب تجاویز اور ٹولز فراہم کرنا اور انہیں ڈیجیٹل طور پر باخبر بنانا تاکہ وہ نہ صرف خود ڈیجیٹل فضا کا مؤثر استعمال کریں بلکہ ان سے شناسا ہوکر اپنے عزیزو اقارب کی مدد بھی کریں۔"
       },
       {
         icon: "umbrella",
         titleEN: "Making Internet Governance Inclusive",
+        textPK: "انٹرنیٹ گورننس کا شامل ہونا",
         textEN: "To set up a new paradigm in Pakistan of Internet governance in Pakistan by making women engage in political discussions with regards to the development of Internet with the terms of fairness, inclusion and respect for women’s rights.",
-        textPK: "To set up a new paradigm in Pakistan of Internet governance in Pakistan by making women engage in political discussions with regards to the development of Internet with the terms of fairness, inclusion and respect for women’s rights."
+        textPK: "انٹرنیٹ کے ارتقا کو لیکر خواتین کے حقوق کیلئے احترام اور ان کی منصفانہ شمولیت کی شرائط کیساتھ سیاسی مباحثوں میں خواتین کو مشغول کر کے پاکستانی انٹرنیٹ گورننس میں ایک نئی انقلابی فکر مرتب کرنا۔"
       },
       {
         icon: "bullhorn",
         titleEN: "Networking for Advocacy",
+        textPK: "حمایت کیلئے مربوط سلسلوں کا قیام",
         textEN: "To partner with media organizations in Pakistan in order to raise awareness around the digital rights of women and advocate for a more women-friendly Internet.",
-        textPK: "To partner with media organizations in Pakistan in order to raise awareness around the digital rights of women and advocate for a more women-friendly Internet."
+        textPK: "پاکستان میں میڈیا اداروں کے ساتھ شراکت داری کرنا تاکہ خواتین کے حقوق سے متعلق آگاہی مہم کا آغاز ہو اور خواتین کو مزید دوستانہ انٹرنیٹ کی حمایت حاصل ہو۔"
       }
     ]
   }
